e4e494618767776c40aed3bd73bc1d3952614bb4
maryam <marychch19@gmail.com>
2024-09-16 20:28:40 +0330

    perfectNumbers

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const numbersArray = [12, 13, 14, 15, 16];
-let sum = 0;
-let cunt = 0;
-for (let i = 0; i < numbersArray.length; i++) {
-  sum += numbersArray[i];
-  cunt++;
+// const numbersArray = [12, 13, 14, 15, 16];
+// let sum = 0;
+// let cunt = 0;
+// for (let i = 0; i < numbersArray.length; i++) {
+//   sum += numbersArray[i];
+//   cunt++;
+// }
+// const avarage = sum / cunt;
+// console.log(`average = `, avarage);
+
+let perfectNumbers = [];
+function isPerfectNumber(num) {
+  if (num <= 0) return;
+  let sum = 0;
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) {
+      sum += i;
+    }
+  }
+  if (sum === num) {
+    perfectNumbers.push(num);
+  }
 }
-const avarage = sum / cunt;
-console.log(`average = `, avarage);
+isPerfectNumber(23);
+isPerfectNumber(6);
+isPerfectNumber(24);
+isPerfectNumber(61);
+isPerfectNumber(28);
+
+console.log(perfectNumbers);
